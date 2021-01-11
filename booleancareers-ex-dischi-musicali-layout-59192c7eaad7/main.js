ab6460cc8ea73bbb76460bf42ab2a633f435feef
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-01-11 14:37:55 +0100

    Copiata la cartella con l'esercizio
    Copiata la cartella con l'esercizio

---
+++ booleancareers-ex-dischi-musicali-layout-59192c7eaad7/main.js
@@ -0,0 +1,7 @@
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali. 
+// Servendoci di Vue JS stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+
+$(document).ready(function() {
+	//Code
+});
